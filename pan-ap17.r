8b48573c244346ad28b2d7ef76f4224b69e2dbe6
jcsola <jcsola@users.noreply.github.com>
2017-06-28 00:53:29 +0200

    Add files via upload

--- pan-ap17.r
+++ pan-ap17.r
@@ -1,9 +1,15 @@
+
+## Instalamos las librerías necesarias ##
 install.packages('qdap')
 install.packages('XML')
 install.packages('tm')
 install.packages('splitstackshape')
 install.packages('kernlab')
 install.packages('e1071')
+#########################################
+
+
+## Una vez instaladas, procedemos a cargarlas ##
 library(e1071)
 library(qdap)
 library(XML)
@@ -11,10 +17,18 @@ library(tm)
 library(splitstackshape)
 library(caret)
 library(kernlab)
-getwd()
+################################################
 
+
+## Establecemos el directorio en el cuál vamos a trabajar ##
+setwd("C:/Users/JC/Desktop/Master Big Data/16. Text Mining en Social Media/Ejercicio Práctico/Entregar/Entregar")
+############################################################
+
+
+## A continuación, generamos el Vocabulario, donde le pasamos el path correspondiente al dataset, que coja 1000 muestras, y le pasamos también signos de puntuación, números, espacios en blanco, etc. ##
 GenerateVocabulary <- function(path, n = 1000, lowcase = TRUE, punctuations = TRUE, numbers = TRUE, whitespaces = TRUE, swlang = "", swlist = "", verbose = TRUE) {
-	setwd(path)
+	
+  setwd(path)
 	
 	files = list.files(pattern="*.xml")
 	
@@ -22,9 +36,9 @@ GenerateVocabulary <- function(path, n = 1000, lowcase = TRUE, punctuations = TR
 	i <- 0
 	for (file in files) {
     # El sexo y variedad correspondan, añadir aqui
-	  #author <- gsub(".xml", "", file)
-	  #variety <- truth[truth$author==author,"variety"]
-	  #gender <- truth[truth$author==author,"gender"]
+	  # author <- gsub(".xml", "", file)
+	  # variety <- truth[truth$author==author,"variety"]
+	  # gender <- truth[truth$author==author,"gender"]
     
     # si se cumple el sexo y variedad entonces ejecutar las cuatro lineas de abajo
     
@@ -41,7 +55,7 @@ GenerateVocabulary <- function(path, n = 1000, lowcase = TRUE, punctuations = TR
 
 	corpus.preprocessed <- corpus.raw
 	
-	
+	# En los "if" siguientes, eliminamos los elementos que no queremos que nos aparezca en el resultado
 	if (lowcase) {
 		if (verbose) print("Tolower...")
 		corpus.preprocessed <- tolower(corpus.preprocessed)
@@ -69,6 +83,7 @@ GenerateVocabulary <- function(path, n = 1000, lowcase = TRUE, punctuations = TR
 		corpus.preprocessed <- removeWords(corpus.preprocessed, swlist)
 	}
   
+	# Eliminamos las letras del abecedario, así como preposiciones y conjunciones.
   corpus.preprocessed <- removeWords(corpus.preprocessed, c("si","vía","ç","a","b","c","d","e","f","g","h","i","j","k","l","m","n","ñ","o","p","q","r","s","t","u","v","w","x","y","z","ante", "con", "contra", "de", "desde", "en", "entre", "cabe", "bajo", "hacia","hasta","para","por","según","sin","segun","so", "sobre","tras","durante","mediante","tras"))
 	
 	if (whitespaces) {
@@ -81,34 +96,34 @@ GenerateVocabulary <- function(path, n = 1000, lowcase = TRUE, punctuations = TR
 	corpus.frequentterms <- freq_terms(corpus.preprocessed, n)
 	if (verbose) plot(corpus.frequentterms)
 	
-  print('--------------------------------------------------------------------------------------------------------------')
-
-  i = 0
+    i = 0
   for (word in corpus.frequentterms[0]){
     # word = substrRight(word, 0)
     word =  substr(word, nchar(word)-1, nchar(word))
     corpus.frequentterms[0][i] = word
     i = i + 1
   }
-  #print(corpus.frequentterms)
+  # print(corpus.frequentterms)
   
 	return (corpus.frequentterms)
 }
 
+
+# En este bloque, generaremos la bolsa de palabras (Bow - Bag of Words) respecto al vocabulario facilitado
 GenerateBoW <- function(path, vocabulary, n = 1000, lowcase = TRUE, punctuations = TRUE, numbers = TRUE, whitespaces = TRUE, swlang = "", swlist = "", class="variety", verbose = TRUE) {
 	setwd(path)
 
-	#Cargamos el fichero de training
+	# Cargamos el fichero de training
 	truth <- read.csv("truth.txt", sep=":", header=FALSE)
 	truth <- truth[,c(1,4,7)]
 	colnames(truth) <- c("author", "gender", "variety")
 
-  #Lista de ficheros que hay en la variables files
+  # Lista de ficheros que hay en la variables files
 	i <- 0
 	bow <- NULL
 	files = list.files(pattern="*.xml")
   
-  #Recorer los ficheros
+  # Recorrer los ficheros
 	for (file in files) {
 		author <- gsub(".xml", "", file)
 		variety <- truth[truth$author==author,"variety"]
@@ -167,17 +182,20 @@ GenerateBoW <- function(path, vocabulary, n = 1000, lowcase = TRUE, punctuations
 	return (bow)
 }
 
-
+# Contamos con 1000 muestras
 n <- 1000
 
-path_training <- "/media/anberbai@alumno.upv.es/NIOAN7 32GB/Master/Asignaturas/16. Text Mining en Social Media/2017/pan-ap17-bigdata/training"
-path_test <- "/media/anberbai@alumno.upv.es/NIOAN7 32GB/Master/Asignaturas/16. Text Mining en Social Media/2017/pan-ap17-bigdata/test"
+# Cargamos los directorios para TRAIN y TEST
+path_training <- "C:/Users/JC/Desktop/Master Big Data/16. Text Mining en Social Media/Ejercicio Práctico/Entregar/Archivos Test y Training/training"
+path_test <- "C:/Users/JC/Desktop/Master Big Data/16. Text Mining en Social Media/Ejercicio Práctico/Entregar/Archivos Test y Training/test"
 
 
+# Guardamos en la variable "vocabulario" todo el vocabulario que hemos generado 
 vocabulary <- GenerateVocabulary(path_training, n, swlang="es")
 
-bow_training <- GenerateBoW(path_training, vocabulary, n, class="variety") #gender variety
-bow_test <- GenerateBoW(path_test, vocabulary, n, class="variety")
+# Recogemos las Bolsas de Palabras para el Train y el Test
+bow_training <- GenerateBoW(path_training, vocabulary, n, class="variety") #gender o variety
+bow_test <- GenerateBoW(path_test, vocabulary, n, class="variety") #gender o variety
 
 training <- cSplit(bow_training, "V1", ",")
 test <- cSplit(bow_test, "V1", ",")
@@ -187,35 +205,37 @@ training2 <- training[,3:ncol(training)]
 #training2 <- training[,ncol(training)]
 training <- cbind(training1,training2)
 
-names(training)[1] <- "class" #columna 1008, es la clase com por ejemplo venezuela training[1, 1008]
-truth  <- unlist(test[,1]) #valores de sexo o variedad de la clase de verdad.
+names(training)[1] <- "class" # columna 1008, es la clase com por ejemplo venezuela training[1, 1008]
+truth  <- unlist(test[,1])    # valores de sexo o variedad de la clase de verdad.
 test <- test[,3:ncol(test)]
 
-#Aprendiendo y evaluando con validacion cruzada.
-#train_control <- trainControl( method="repeatedcv", number = 10 , repeats = 3) 
-#model_SVM <- train( class~., data= training, trControl = train_control, method = "svmLinear")
-#print(model_SVM)
+# Aprendiendo y evaluando con validacion cruzada.
+# train_control <- trainControl( method="repeatedcv", number = 10 , repeats = 3) 
+# model_SVM <- train( class~., data= training, trControl = train_control, method = "svmLinear")
+# print(model_SVM)
 
-#or 
+# or 
 
 ## All subsequent models are then run in parallel
 model_SVM <- train(class ~ ., data = training, method = "rf", ntree=100)
 print(model_SVM)
 
-#Aprendiendo con todo el training
-#train_control <- trainControl(method="none")
-#model_SVM <- train( class~., data= training, trControl = train_control, method = "svmLinear")
-#print(model_SVM)
-#predictores son caracteristicas, es decir, la "n"
-#clases.
-#Accurasy es variety
-#kappa mas cercano a 0, significa que es debido al azar.
+# Aprendiendo con todo el training
+# train_control <- trainControl(method="none")
+# model_SVM <- train( class~., data= training, trControl = train_control, method = "svmLinear")
+# print(model_SVM)
+# predictores son caracteristicas, es decir, la "n" clases.
+# Accuracy es variety
+# kappa mas cercano a 0, significa que es debido al azar.
 
 #Evaluando con el test
 pred_SVM <- predict(model_SVM, test)
+
+# Creamos la Matriza de Confusión, la cual nos mostrará el Accuracy obtenido
 confusionMatrix(pred_SVM, truth)
 
 
+######## PRUEBAS REALIZADAS ########
 
 #######
 #training <- training[,3:(n+3)]
@@ -223,16 +243,16 @@ confusionMatrix(pred_SVM, truth)
 #truth  <- unlist(test[,(n+3):(n+3)]) #valores de sexo o variedad de la clase de verdad.
 #######
 
+
 #######
 #training <- cSplit(bow_training, "V1", ",")
 #test <- cSplit(bow_test, "V1", ",")
-#
+
 #training <- training[,3:1003]
 #test <- test[,1:1002]
 #pred_SVM <- predict(model_SVM, test)
 #confusionMatrix(pred_SVM, truth)
 ####### 
-
 
 
 #corpus.frequentterms >- freq_terms(corpus.raw,n)
